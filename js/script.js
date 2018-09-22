836336564ca8b97177c3286e0921806f93b36409
Matheus Negrão <matheus.dnegrao@gmail.com>
2018-09-22 14:42:47 -0300

    Primeiro Commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+function exibeMenu(){
+    var menu = document.getElementsByClassName('menu-principal')[0];
+    if(menu.style.display == 'none')
+        menu.style.display = 'block';
+    else
+        menu.style.display = 'none';
+};
+
+document.body.onresize = function(){
+    var w = window.outerWidth;
+    var menu = document.getElementsByClassName("menu-principal")[0];
+    if (w >= 1000){
+        menu.style.display = 'block';
+    } else{
+        menu.style.display = 'none';        
+    }
+};
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+};
+
+document.getElementById("defaultOpen").click();
